d124e52fb9ce750c14add7adff9fa24635f4dfdc
apsara123127 <apsaraaitrich@gmail.com>
2025-11-20 14:14:17 +0530

    added

---
+++ MVC/Activities/PartialViewDemo/PartialViewDemo/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewDemo.Models;
+
+namespace PartialViewDemo.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private static readonly List<Student> _students = new()
+{
+        new Student { Id = 1, Name = "Anu", Course = "C#" },
+        new Student { Id = 2, Name = "Balu", Course = "ASP.NET Core" },
+        new Student { Id = 3, Name = "Chris", Course = "SQL" }
+};
+
+
+        public IActionResult Index()
+        {
+            // Send the list to the view
+            return View(_students);
+        }
+    }
+}
