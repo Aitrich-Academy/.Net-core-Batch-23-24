eb511955187509f3dffdd3b00bf274e8b5f72506
apsara123127 <apsaraaitrich@gmail.com>
2025-12-09 17:04:43 +0530

    added

---
+++ Web API/WebApi_Login/WebApi_Login/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using WebApi_Login.DTOs;
+
+namespace WebApi_Login.Services
+{
+    public interface IUserService
+    {
+        string Register(UserRegisterDto userDto);
+        object Login(UserLoginDto loginDto);
+    }
+}
