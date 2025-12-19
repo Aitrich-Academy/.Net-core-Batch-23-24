daca619db7d30347e680254fd5deb137e9d5330b
Chinnu <chinnuts1994@gmail.com>
2025-12-19 10:44:03 +0100

    machinetest

---
+++ MVC/Chinnu-MVC/MachineTest_REG_BOOk/MachineTest_REG_BOOk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MachineTest_REG_BOOk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
